ba1db194575bcc21f2b73af1f791294263acdcf1
Yurii Trufanov <trufanowyura@gmail.com>
2020-11-15 00:59:18 +0200

    ДЗ урок 2 доработка

--- core/src/ru/gb/dungeon/Hero.java
+++ core/src/ru/gb/dungeon/Hero.java
@@ -10,57 +10,24 @@ import com.badlogic.gdx.math.Vector2;
 public class Hero {
     private ProjectileController projectileController;
     private Vector2 position;
-    private Vector2 dir_left_right;
-    private Vector2 dir_up_down;
+    private float velocity = 150.0f;
     private TextureRegion texture;
     private boolean doubleFire = false;
     private float angle;
-    private float speed = 5.0f;
+    private float halfSize;
+
+
 
     public Hero(TextureAtlas atlas, ProjectileController projectileController) {
         this.position = new Vector2(100, 200);
         this.texture = atlas.findRegion("tank");
         this.projectileController = projectileController;
-        this.dir_left_right = new Vector2(speed, 0);
-        this.dir_up_down = new Vector2(0, speed);
+        this.halfSize = texture.getRegionHeight() / 2.0f;
+
     }
 
-//    public void fire(boolean doubleFire, float angle) {
-//        if (!doubleFire){
-//            if (angle == 0) {
-//                projectileController.activate(position.x, position.y, 400, 0);
-//            }
-//            if (angle == 90) {
-//                projectileController.activate(position.x, position.y, 0, 400);
-//            }
-//            if (angle == 180) {
-//                projectileController.activate(position.x, position.y, -400, 0);
-//            }
-//            if (angle == 270) {
-//                projectileController.activate(position.x, position.y, 0, -400);
-//            }
-//        } else {
-//            if (angle == 0) {
-//                projectileController.activate(position.x, position.y, 400, 10);
-//                projectileController.activate(position.x, position.y, 400, -10);
-//            }
-//            if (angle == 90) {
-//                projectileController.activate(position.x, position.y, 10, 400);
-//                projectileController.activate(position.x, position.y, -10, 400);
-//            }
-//            if (angle == 180) {
-//                projectileController.activate(position.x, position.y, -400, 10);
-//                projectileController.activate(position.x, position.y, -400, -10);
-//            }
-//            if (angle == 270) {
-//                projectileController.activate(position.x, position.y, 10, -400);
-//                projectileController.activate(position.x, position.y, -10, -400);
-//            }
-//        }
-//    }
-
     public void fire(boolean doubleFire, float angle) {
-        if (!doubleFire){
+        if (!doubleFire) {
             if (angle == 0) {
                 projectileController.activate(position.x, position.y, 400, 0);
             }
@@ -102,40 +69,38 @@ public class Hero {
         }
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             angle = 90.f;
-            position.add(dir_up_down);
-            if (position.y > 720) {
-                position.y = 720;
+            position.y += velocity * dt;
+            if (position.y > 720 - halfSize) {
+                position.y = 720 - halfSize;
             }
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
             angle = 270.f;
-            position.sub(dir_up_down);
-            if (position.y < 0) {
-                position.y = 0;
+            position.y -= velocity * dt;
+            if (position.y < 0 + halfSize) {
+                position.y = 0 + halfSize;
             }
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             angle = 180.f;
-            position.sub(dir_left_right);
-            if (position.x < 0) {
-                position.x = 0;
+            position.x -= velocity * dt;
+            if (position.x < 0 + halfSize) {
+                position.x = 0 + halfSize;
             }
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             angle = 0.f;
-            position.add(dir_left_right);
-            if (position.x > 1280) {
-                position.x = 1280;
+            position.x += velocity * dt;
+            if (position.x > 1280 - halfSize) {
+                position.x = 1280 - halfSize;
             }
-            return;
         }
     }
 
     public void render(SpriteBatch batch) {
-//        batch.draw(texture, position.x - 20, position.y - 20);
         batch.draw(texture, position.x - 20, position.y - 20, 20, 20, 40, 40, 1, 1, angle);
     }
 }
